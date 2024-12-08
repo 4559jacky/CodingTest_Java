d907e23d5d1f813d18f777c30fcd3a1a393294ef
4559jacky <4559jacky@gmail.com>
2024-12-08 18:37:59 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int n) {
+        int size = 0;
+        if(n%2==0) {
+            size = n/2;
+        } else {
+            size = n/2 + 1;
+        }
+        int[] answer = new int[size];
+        int a = 0;
+        for(int i=1; i<=n; i++) {
+            if(i%2!=0) {
+                answer[a++] = i;
+            }
+        }
+        return answer;
+    }
+}
