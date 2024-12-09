3288ede7bd36f8b6dcaf715dc4357f5fd849a8eb
4559jacky <4559jacky@gmail.com>
2024-12-09 20:20:42 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 11.26 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        my_string = my_string.toLowerCase();
+        char[] c = my_string.toCharArray();
+        Arrays.sort(c);
+        for(char a : c) {
+            answer += a;
+        }
+        return answer;
+    }
+}
