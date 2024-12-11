8dcf3c6ea202f187ab9b9615e167bf84fd498157
4559jacky <4559jacky@gmail.com>
2024-12-12 00:48:24 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.16 ms, Memory: 91.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String[] str = s.split(" ");
+        int sum = 0;
+        for(int i=0; i<str.length; i++) {
+            if(str[i].equals("Z")) {
+                sum -= Integer.parseInt(str[i-1]);
+            } else {
+                sum += Integer.parseInt(str[i]);
+            }
+            
+        }
+        return sum;
+    }
+}
