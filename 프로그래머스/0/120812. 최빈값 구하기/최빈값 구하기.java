1dc1c8c4901da5504e5184270f976de889bd3a6b
4559jacky <4559jacky@gmail.com>
2024-12-18 16:37:30 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.20 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -2,11 +2,12 @@ class Solution {
     public int solution(int[] array) {
         int answer = -1;
         int max = 0;
+        int count = 0;
         if(array.length==1) {
         	answer = array[0];
-        }
+        } 
         for(int i=0; i<array.length-1; i++) {
-            int count = 0;
+            count = 0;
             for(int j=i+1; j<array.length; j++) {
                 if(array[i]==array[j]) {
                     count++;
