82670ecf028f28a548d6b8b460eb70ba32cbcb86
4559jacky <4559jacky@gmail.com>
2025-02-16 21:21:10 +0900

    [level 0] Title: 커피 심부름, Time: 0.73 ms, Memory: 96.6 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -2,8 +2,7 @@ class Solution {
     public int solution(String[] order) {
         int answer = 0;
         for(String o : order) {
-            if(o.equals("iceamericano") || o.equals("americanoice") || o.equals("americano") ||
-              o.equals("hotamericano") || o.equals("americanohot") || o.equals("anything")) {
+            if(o.contains("americano") || o.equals("anything")) {
                 answer += 4500;
             } else {
                 answer += 5000;
